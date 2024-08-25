95213d3f5b1e89469c6de54f4a5800c30fe1fff7
Omor Faruk Mahfuj <mypcfaruk+github@gmail.com>
2024-08-26 04:19:51 +0600

    Task 10 Completed

---
+++ Task 10/script.js
@@ -0,0 +1,8 @@
+window.onscroll = function() {updateProgressBar()};
+
+function updateProgressBar() {
+    const winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+    const height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    const scrolled = (winScroll / height) * 100;
+    document.getElementById("progressBar").style.width = scrolled + "%";
+}
--- readme.md
+++ readme.md
@@ -17,3 +17,5 @@
 8. Create a search bar that displays live search results as users type, updating the results without requiring a full page reload.
 
 9. Build a character counter for a text area or input field, which updates in real time when user types and enforces a character limit.
+
+10. Show a progress bar which shows how much page has been scrolled.
